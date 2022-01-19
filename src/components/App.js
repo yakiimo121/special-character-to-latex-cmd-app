95dc67c793f52524665e34387f49932fa9e9a333
yoshidatakuto <takuto@ucl.nuee.nagoya-u.ac.jp>
2022-01-19 16:52:13 +0900

    update

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,19 @@
+import React from 'react';
 import './App.css';
 import Form from './Form';
-import Table from './Table';
+import LatexCmdTable from './LatexCmdTable';
 
 function App() {
   return (
     <div>
       <Form />
       <hr size="1" wifth="100%" color="gray"></hr>
-      <Table />
-      <h2>参考</h2>
+      <LatexCmdTable />
+      {/* <h2>参考</h2>
       <ul>
         <li><a href="http://www.bibtex.org/SpecialSymbols/">BibTeX Special Symbols</a></li>
         <li><a href="https://cosmoshouse.com/tools/acc-conv-j.htm">アクセント付き文字の変換表 0.11（アクセント変換表）</a></li>
-      </ul>
+      </ul> */}
     </div>
   );
 }
