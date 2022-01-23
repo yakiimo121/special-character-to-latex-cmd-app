eeb4d43f6ce0554fbf619ef61af42388de8b09c3
takuto <takuto@ucl.nuee.nagoya-u.ac.jp>
2022-01-23 19:45:03 +0900

    refactor

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import Form from './Form';
 import LatexCmdTable from './LatexCmdTable';
 
@@ -9,11 +8,6 @@ function App() {
       <Form />
       <hr size="1" wifth="100%" color="gray"></hr>
       <LatexCmdTable />
-      {/* <h2>参考</h2>
-      <ul>
-        <li><a href="http://www.bibtex.org/SpecialSymbols/">BibTeX Special Symbols</a></li>
-        <li><a href="https://cosmoshouse.com/tools/acc-conv-j.htm">アクセント付き文字の変換表 0.11（アクセント変換表）</a></li>
-      </ul> */}
     </div>
   );
 }
--- src/components/Form.js
+++ src/components/Form.js
@@ -5,12 +5,10 @@ import latexCmdJson from '../latex-cmd.json';
 
 
 function Form() {
-  const [inText, setInText] = useState("");
   const [outText, setOutText] = useState("");
   let tmpText = "";
 
   const handleChange = (event) => {
-    setInText(event.target.value);
     tmpText = event.target.value;
     Object.keys(latexCmdJson).forEach(function (key) {
       tmpText = tmpText.replace(RegExp(key, 'g'), latexCmdJson[key]);
@@ -21,32 +19,28 @@ function Form() {
   return (
     <div>
       <div>
-        {/* <textarea className="textarea-large" type="text" name="input text" value={inText} onChange={handleChange}></textarea> */}
         <TextField 
           multiline 
           focused 
           className="textfield-large" 
           label="LaTeX (including special characters)"
-          name="input text"
-          value={inText} 
+          name="input field"
           variant="filled"
           rows={6}
           margin="normal"
           onChange={handleChange}/>
       </div>
       <div>
-        {/* <textarea readOnly className="textarea-large" type="text" name="output text" value={outText}></textarea> */}
         <TextField 
           multiline 
           focused 
           className="textfield-large" 
           label="LaTeX (using escaped codes)"
-          name="output text" 
+          name="output field" 
           value={outText} 
           variant="filled"
           rows={6}
           margin="normal"
-          onChange={handleChange}
           InputProps={{
             readOnly: true,
           }}/>
